f57fd7b428575075636cd0fe712466ef6cb0cb16
sw-yx <swyx@dontemail.me>
2019-09-06 16:56:05 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [text, setText] = React.useState('')
+  React.useEffect(() => {
+    fetch('/.netlify/functions/node-fetch')
+      .then((x) => x.json())
+      .then((x) => setText(x.value))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>{text}</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
